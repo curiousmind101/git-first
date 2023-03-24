1b94e2daf7b26605806bfa913dc4163cd4c29b02
Ajit Kumar <ajitkumar.nitrr@gmail.com>
2023-03-24 11:22:36 +0530

    hello.cpp is modified

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
-cout<<"This File is Created using cmd with Notepad Command."<<endl;
-return 0;
+    cout<<"This File is Created using cmd with Notepad Command."<<endl;
+    cout<<"Now this line of code is add(modified) after git commit"<<endl;
+    return 0;
 }
